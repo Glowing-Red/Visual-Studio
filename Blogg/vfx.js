15d9f58ab5d74ede6fc8bb9d5fb93660637e23c6
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-11-07 09:34:16 +0100

    Upload

---
+++ Blogg/vfx.js
@@ -0,0 +1,66 @@
+var header = document.getElementById("navigation");
+var sticky = header.offsetTop - header.offsetHeight / 2;
+document.body.addEventListener("scroll", function() {
+    if (document.body.scrollTop > sticky) {
+        header.classList.add("sticky-navigation");
+    } else {
+        header.classList.remove("sticky-navigation");
+    }
+});
+
+document.querySelectorAll(".nav-item").forEach((item) => {
+    getComputedStyle(item, ':hover');
+    const Colour = item.style.color;
+    item.addEventListener('mousedown', () => {
+        item.style.color = `rgba(255, 0, 0, 1)`;
+    });
+    item.addEventListener('mouseup', () => {
+        item.style.color = Colour;
+    });
+    item.addEventListener('mouseleave', () => {
+        item.style.color = Colour;
+    });
+});
+
+const About = document.querySelector(".About-me");
+if (About) {
+    const About_A = About.querySelector('a');
+    const About_h4 = About.querySelector('h4');
+
+    About_A.addEventListener('mousedown', () => {
+        About.style.boxShadow = `0 0 25px #920002`;
+        About_A.style.backgroundColor = `#920002`;
+        About_h4.style.textShadow = `0 0 10px #920002`;
+    });
+    About_A.addEventListener('mouseup', () => {
+        About.style.boxShadow = `0 0 10px #cd0003`;
+        About_A.style.backgroundColor = `#cd0003`;
+        About_h4.style.textShadow = `0 0 15px #cd0003`;
+    });
+    About_A.addEventListener('mouseenter', () => {
+        About.style.boxShadow = `0 0 20px #B80104`;
+        About_A.style.backgroundColor = `#B80104`;
+        About_h4.style.textShadow = `0 0 5px #B80104`;
+    });
+    About_A.addEventListener('mouseleave', () => {
+        About.style.boxShadow = `0 0 10px #cd0003`;
+        About_A.style.backgroundColor = `#cd0003`;
+        About_h4.style.textShadow = `0 0 15px #cd0003`;
+    });
+}
+
+document.querySelectorAll('.Continue-Reading').forEach((Continue) => {
+    getComputedStyle(Continue, '::before');
+    Continue.addEventListener('mousedown', () => {
+        Continue.style.setProperty('--before-filter', `blur(5.5px)`);
+    });
+    Continue.addEventListener('mouseup', () => {
+        Continue.style.setProperty('--before-filter', `blur(4px)`);
+    });
+    Continue.addEventListener('mouseenter', () => {
+        Continue.style.setProperty('--before-filter', `blur(4px)`);
+    });
+    Continue.addEventListener('mouseleave', () => {
+        Continue.style.setProperty('--before-filter', `blur(2.5px)`);
+    });
+});
