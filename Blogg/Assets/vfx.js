7a097388a0e315a4356a2fced05fe9987891e927
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2023-11-21 11:50:22 +0100

    Responsivity
    A little bit of Responsive :D

--- Blogg/Assets/vfx.js
+++ Blogg/Assets/vfx.js
@@ -1,32 +1,44 @@
 document.querySelectorAll(".nav-item").forEach((item) => {
-    getComputedStyle(item, ':hover');
     const Colour = item.style.color;
+
     item.addEventListener('mousedown', () => {
         item.style.color = `rgba(255, 0, 0, 1)`;
     });
     item.addEventListener('mouseup', () => {
         item.style.color = Colour;
     });
+    item.addEventListener('mouseover', function(event) {
+        item.style.color = `rgba(0, 0, 0, 0.65)`;
+    });
     item.addEventListener('mouseleave', () => {
+        item.style.color = Colour;
+    });
+
+    item.addEventListener('touchstart', () => {
+        item.style.color = `rgba(255, 0, 0, 1)`;
+    });
+    item.addEventListener('touchend', () => {
         item.style.color = Colour;
     });
 });
 
-const About = document.querySelector(".About-me");
-if (About) {
+document.querySelectorAll(".About-me").forEach((About) => {
     const About_A = About.querySelector('a');
     const About_h4 = About.querySelector('h4');
 
-    About_A.addEventListener('mousedown', () => {
+    function down() {
         About.style.boxShadow = `0 0 25px #920002`;
         About_A.style.backgroundColor = `#920002`;
         About_h4.style.textShadow = `0 0 10px #920002`;
-    });
-    About_A.addEventListener('mouseup', () => {
+    }
+    function up() {
         About.style.boxShadow = `0 0 10px #cd0003`;
         About_A.style.backgroundColor = `#cd0003`;
         About_h4.style.textShadow = `0 0 15px #cd0003`;
-    });
+    }
+
+    About_A.addEventListener('mousedown', down);
+    About_A.addEventListener('mouseup', up);
     About_A.addEventListener('mouseenter', () => {
         About.style.boxShadow = `0 0 20px #B80104`;
         About_A.style.backgroundColor = `#B80104`;
@@ -37,7 +49,10 @@ if (About) {
         About_A.style.backgroundColor = `#cd0003`;
         About_h4.style.textShadow = `0 0 15px #cd0003`;
     });
-}
+    
+    About_A.addEventListener('touchstart', down);
+    About_A.addEventListener('touchend', up);
+});
 
 document.querySelectorAll('.Continue-Reading').forEach((Continue) => {
     getComputedStyle(Continue, '::before');
